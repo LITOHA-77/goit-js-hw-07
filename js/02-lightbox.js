a3158012e478003cbf9ddca095e0bdf018588e5f
Maksym Lytovchenko <lmb1988@ukr.net>
2022-08-13 14:54:08 +0300

    hw-7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const listForRender = document.querySelector(".gallery");
+const imageMarkup = createGalleryMarkup(galleryItems);
+listForRender.insertAdjacentHTML("beforeend", imageMarkup);
+
+function createGalleryMarkup() {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`;
+    })
+    .join("");
+}
+
+listForRender.addEventListener("click", onListForRenderClick);
+
+function onListForRenderClick(e) {
+  e.preventDefault();
+
+  let gallery = new SimpleLightbox(".gallery a");
+  gallery.on("show.simplelightbox", function () {});
+}
